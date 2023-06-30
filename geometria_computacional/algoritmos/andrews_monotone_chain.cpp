3d9fa8fcc6a1b6bd8be7b0b1f2e331c513ea1df9
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2023-06-30 06:28:55 -0300

    Update andrews_monotone_chain.cpp

--- geometria_computacional/algoritmos/andrews_monotone_chain.cpp
+++ geometria_computacional/algoritmos/andrews_monotone_chain.cpp
@@ -1,83 +1,82 @@
 /* Universidade de Brasília
  * Departamento de Ciência da Computação
- * CIC0xxx - Tópicos Especiais em Programação Competitiva
+ * CIC0258 - Tópicos Especiais em Programação Competitiva
  * Prof. Dr. Vinicius R. P. Borges
  * 
  * Tópico: Geometria Computacional, Fecho Convexo (Convex Hull)
  * Objetivo: Implementação do Algoritmo Cadeia Monótona de Andrews
  * 
- * Compilar no terminal: g++ andrews_monotone_chain.cpp -std=c++11 -o convexhull
+ * Compilar no terminal: g++ andrews_monotone_chain.cpp -std=c++17 -o convexhull
  * Executar: ./convexhull
  */
 
 #include<bits/stdc++.h>
-
+ 
 using namespace std;
 
+typedef long long ll;
+ 
 typedef struct point Point2D;
-
-struct point
-{
+ 
+struct point{
     double x;
     double y;
     
-    bool operator<(Point2D p)
-    {
+    bool operator<(Point2D p){
         return x < p.x || (x == p.x && y < p.y);
     }
 };
-
-// v=pq comparar p/ determinar se r estah a esq, direita ou col em relacao a v
-double prod_vetorial(Point2D p, Point2D q, Point2D r)
-{
-    return (p.x-r.x)*(q.y-r.y) - (p.y-r.y)*(q.x-r.x);
+ 
+// verifica se o ponto r estah a esq, direita ou col em relacao ao vetor u = (q-p)
+double cross(Point2D p, Point2D q, Point2D r){
+    double rx = r.x-p.x;
+    double ry = r.y-p.y;
+    
+    double ux = q.x-p.x;
+    double uy = q.y-p.y;
+    
+    return rx*uy - ry*ux;
 }
-
-vector<Point2D> andrews_convexhull(vector<Point2D> points)
-{
-    int tam_lower,tam_upper;
-    sort(points.begin(),points.end());
+ 
+vector<Point2D> andrews_convexhull(vector<Point2D> points){
+    
+    vector<Point2D> hull;
+    int tam_upper;
     int n = points.size();
     
-    vector<Point2D> lower,upper;
+    // ordena crescente em relacao ao eixo x
+    sort(points.begin(),points.end());
     
     // pega ponto por ponto ordenado crescente em rel. ao eixo x
-    for(int i = 0; i < n; i++)
-    {
-        tam_lower = lower.size();
-        
-        while(tam_lower >= 2 && prod_vetorial(lower[tam_lower-2],lower[tam_lower-1],points[i]) <= 0)
-        {
-            lower.pop_back();
-            tam_lower--;
+    for(int i = 0; i < n; i++){
+        while((int) hull.size() >= 2){
+            double pv = cross(hull[(int) hull.size()-2],hull[(int) hull.size()-1],points[i]);
+            if(pv <= 0)
+                break;
+            hull.pop_back();
         }
-        lower.push_back(points[i]);
+        hull.push_back(points[i]);
     }
-    // lower hull
     
     reverse(points.begin(),points.end());
+    hull.pop_back();
+    tam_upper = hull.size();
     
-    // upper hull
-    for(int i = 0; i < n; i++)
-    {
-        tam_upper = upper.size();
-        
-        while(tam_upper >= 2 && prod_vetorial(upper[tam_upper-2],upper[tam_upper-1],points[i]) <= 0)
-        {
-            upper.pop_back();
-            tam_upper--;
+    // lower hull
+    for(int i = 0; i < n; i++){       
+        while(hull.size() - tam_upper >= 2){
+            double pv = cross(hull[(int) hull.size()-2],hull[(int) hull.size()-1],points[i]);
+            if(pv <= 0)
+                break;
+            hull.pop_back();
         }
-        upper.push_back(points[i]);
+        hull.push_back(points[i]);
     }
-    
-    lower.pop_back();
-    lower.insert(lower.end(),upper.begin(),upper.end());
-    return lower;
+    hull.pop_back();
+    return hull;
 }
 
-
-int main()
-{
+int main(){
     vector<Point2D> point_cloud;
     
     point_cloud.push_back({0,4});
@@ -88,12 +87,11 @@ int main()
     point_cloud.push_back({4,0});
     point_cloud.push_back({4,4});
     point_cloud.push_back({0,0});
-
+    
     vector<Point2D> ans = andrews_convexhull(point_cloud);
     
-    for(int i = 0; i < ans.size(); i++)
-    {
-        printf("(%lf,%lf)\n",ans[i].x,ans[i].y);
+    for(int i = 0; i < ans.size(); i++){
+        printf("%lld %lld\n", (ll) ans[i].x, (ll) ans[i].y);
     }
     
     return 0;
