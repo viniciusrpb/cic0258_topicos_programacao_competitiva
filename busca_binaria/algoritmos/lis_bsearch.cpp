e88a820933711737ea991b73dc6717b379749758
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-01-10 11:23:53 -0300

    Update lis_bsearch.cpp

--- busca_binaria/algoritmos/lis_bsearch.cpp
+++ busca_binaria/algoritmos/lis_bsearch.cpp
@@ -17,38 +17,69 @@
 
 #include<bits/stdc++.h>
 #define MAX 200001
- 
+
 using namespace std;
 
-int lis_nlogn(int *v, int n){
- 
-    int bucket[n+1];
+int pred[200001],ind_bucket[200001];
+
+int lis_binsearch(int *v, int n){
+
+    int bucket[200001];
+
     bucket[0] = v[0];
-    int ans = 1;
+    ind_bucket[0] = 0;
+
+    pred[0] = -1;
+
+    int k = 1;
     for(int i = 1; i < n; i++){
-        if(v[i] > bucket[ans-1])
-            bucket[ans++] = v[i];
+        if(v[i] > bucket[k-1]){
+            pred[i] = ind_bucket[k-1];
+            ind_bucket[k] = i;
+            bucket[k] = v[i];
+            k++;
+        }
         else{
-            int index = lower_bound(bucket,bucket+ans,v[i])-bucket;
-            bucket[index]=v[i];
+            int index = lower_bound(bucket,bucket+k,v[i])-bucket;
+            bucket[index] = v[i];
+            ind_bucket[index] = i;
+
+            if(index > 0)
+                pred[i] = ind_bucket[index-1];
+            else
+                pred[i] = -1;
         }
     }
- 
-    return ans;
+
+    return k;
 }
- 
+
 int main(){
- 
-    int n;
-    int array[MAX+1];
- 
+
+    int ans,n;
+    int arr[MAX];
+    vector<int> subsequence;
+
     scanf("%d",&n);
- 
+
     for(int i = 0; i < n; i++){
-        scanf("%d",array+i);
+        scanf("%d",&arr[i]);
     }
- 
-    printf("%d\n",lis_nlogn(array,n));
- 
+
+    ans = lis_binsearch(arr,n);
+    printf("%d\n",ans);
+
+    int index = ind_bucket[ans-1];
+    while(index != -1) {
+        subsequence.push_back(arr[index]);
+        index = pred[index];
+    }
+
+    reverse(subsequence.begin(), subsequence.end());
+    for(int i = 0; i < subsequence.size(); i++){
+        printf("%d ", subsequence[i]);
+    }
+    printf("\n");
+
     return 0;
 }
