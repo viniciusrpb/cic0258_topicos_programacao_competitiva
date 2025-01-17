1e7794c26f27f00d667a20886e398a5dbd4fa8d5
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-01-17 09:29:22 -0300

    Update dp_digitos.cpp

--- programacao_dinamica/algoritmos/dp_digitos.cpp
+++ programacao_dinamica/algoritmos/dp_digitos.cpp
@@ -16,72 +16,74 @@ using namespace std;
 
 typedef long long ll;
 
-ll tab[20][2][180];
+ll tab[20][2][200];
 
-void getDigits(int n, vector<int>& digits){
+void num2digits(int n, vector<int>& digits){
     while(n){
         digits.push_back(n%10);
-        n=n/10;
+        n = n/10;
     }
 }
 
-ll dp(int idx, int tight, int sum, vector<int>& digits){
-    int k, newtight;
-    
-    if(idx == -1){
-        return sum;
-    }
-    
-    if(tab[idx][tight][sum] != -1 and tight != 1){
-        return tab[idx][tight][sum];
-    }
-    
-    ll ans = 0;
-    if(tight == 1)
-        k = digits[idx];
-    else
-        k = 9;
-    
-    for(int i = 0; i <= k ; i++){
-        newtight = 0;
-        if(digits[idx] == i)
-            newtight = tight;
+ll dp(int idx, int trava, int soma, vector<int>& digits){
 
-        ans+=dp(idx-1,newtight,sum+i,digits);
+    if(idx == -1)
+        return soma;
+
+    if(tab[idx][trava][soma] != -1 and trava != 1){
+        return tab[idx][trava][soma];
     }
-    
-    if(!tight)
-        tab[idx][tight][sum]=ans;
-    
+
+    int max_digit,next_trava;
+
+    if(trava == 1)
+        max_digit = digits[idx];
+    else
+        max_digit = 9;
+
+    ll ans = 0;
+
+    // i : valor literal do digito
+    for(int i = 0; i <= max_digit; i++){
+        next_trava = 0;
+        if(trava == 1 && i == max_digit){
+            next_trava = 1;
+        }
+        ans += dp(idx-1,next_trava,soma+i,digits);
+    }
+
+    if(trava == 0)
+        tab[idx][trava][soma] = ans;
+
     return ans;
 }
 
-ll solve(int l, int r){
-    
-    memset(tab, -1, sizeof(tab));
-    
-    vector<int> digitsL;
-    getDigits(l-1,digitsL);
-    
-    ll ans_l = dp(digitsL.size()-1,1,0,digitsL);
-    
-    vector<int> digitsR;
-    getDigits(r,digitsR);
-    
-    ll ans_r = dp(digitsR.size()-1,1,0,digitsR);
-    
-    return ans_r - ans_l;
-    
+ll solve(int a, int b){
+
+    // inicializacao da tabela
+    memset(tab,-1,sizeof(tab));
+
+    vector<int> digitsA;
+    num2digits(a-1,digitsA);
+    ll sum_a = dp(digitsA.size()-1,1,0,digitsA); // completar depois;
+
+    vector<int> digitsB;
+    num2digits(b,digitsB);
+    ll sum_b = dp(digitsB.size()-1,1,0,digitsB); // completar depois;
+
+    return sum_b - sum_a;
 }
 
 int main(){
-    int t,a,b;
-    
+    int t;
+    ll a,b;
+
     scanf("%d",&t);
-    
+
     while(t--){
-        scanf("%d %d",&a,&b);
+        scanf("%lld %lld",&a,&b);
         printf("%lld\n",solve(a,b));
     }
+
     return 0;
 }
