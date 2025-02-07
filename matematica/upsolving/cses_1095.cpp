76f0f4ea9ff803a8e94858554b3c2699bda1338e
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-02-07 06:45:47 -0300

    Update cses_1095.cpp

--- matematica/upsolving/cses_1095.cpp
+++ matematica/upsolving/cses_1095.cpp
@@ -3,28 +3,27 @@
 using namespace std;
  
 typedef long long ll;
- 
-const ll mod = 1000000007LL;
- 
-ll fast_exp_mod(ll a, ll b){
+
+ll fast_exp_mod(ll a, ll b, ll mod){
     
     if(b == 0)
         return 1;
     
     if(b%2)
-        return a*fast_exp_mod(a*a%mod, b/2)%mod;
+        return a*fast_exp_mod(a*a%mod, b/2, mod)%mod;
     else
-        return fast_exp_mod(a*a%mod, b/2);
-    
+        return fast_exp_mod(a*a%mod, b/2, mod);
 }
  
 int main(){
-    int n,a,b;
+     int n,a,b;
     
-    scanf("%d",&n);
+     scanf("%d",&n);
     
-    while(n--){
-        scanf("%d %d",&a,&b);
-        printf("%lld\n",fast_exp_mod(a,b));
-    }
+     while(n--){
+         scanf("%d %d",&a,&b);
+         printf("%lld\n",fast_exp_mod(a,b,1000000007LL));
+     }
+
+     return 0;
 }
