1e4bbdd888b4aa35fc66c02eaddb4b8d2265d10e
Vinicius R. P. Borges <viniciusrpb@gmail.com>
2025-02-07 11:59:41 -0300

    Update inv_mult.cpp

--- matematica/general/inv_mult.cpp
+++ matematica/general/inv_mult.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+
+ll gcd_extended(ll a, ll b, ll *x, ll *y){
+    if(b == 0){
+        *x = 1;
+        *y = 0;
+        return a;
+    }
+    else{
+        ll d = gcd_extended(b, a%b, x, y);
+        ll xx = *x;
+        ll yy = *y;
+        *x = yy;
+        *y = xx - (a/b)*yy;
+        return d;
+    }
+}
+
+ll inverso_multiplicativo(ll a, ll b){
+      ll x, y;
+      d = gcd_extended(a, b,&x,&y);
+}
+
+int main(){
+
+    
+    return 0;
+}
